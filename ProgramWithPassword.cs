49592aa8c4b0728a3b76b606f0891dd52d060284
mikaa2001 <mikaa2001@gmail.com>
2024-02-28 10:18:17 +0500

    Program with a password

---
+++ ProgramWithPassword.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ProgramWithPassword
+{
+	internal class ProgramWithPassword
+	{
+		static void Main(string[] args)
+		{
+			int numberOfAttempts = 3;
+			string password = "password";
+
+			Console.WriteLine($"У вас есть {numberOfAttempts} попыток, чтобы ввести верный пароль");
+
+			for (int i = 0; i < numberOfAttempts; ++i)
+			{
+				Console.Write("Введите пароль: ");
+				string enteredPassword = Console.ReadLine();
+
+				if (enteredPassword == password)
+				{
+					Console.WriteLine("Секретное сообщение");
+					break;
+				}
+				else
+				{
+					int attemptsLeft = numberOfAttempts - i - 1;
+					Console.WriteLine($"Неверный пароль. У вас осталось попыток {attemptsLeft}");
+				}
+			}
+		}
+	}
+}
